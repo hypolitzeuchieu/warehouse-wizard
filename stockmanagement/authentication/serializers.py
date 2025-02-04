01273d33dd32f66690456b54e86ce682497b5531
hypolitzeuchieu <hypolitzeuchieu@gmail.com>
2025-02-04 11:12:51 +0100

    update serializer

--- stockmanagement/authentication/serializers.py
+++ stockmanagement/authentication/serializers.py
@@ -43,52 +43,6 @@ class RegisterUserSerializer(serializers.ModelSerializer):
         return user
 
 
-class RegisterWholesaleClientSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = User
-        fields = [
-            'username',
-            'email',
-            'phone_number',
-            'password',
-        ]
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            phone_number=validated_data['phone_number'],
-            password=validated_data['password'],
-            role='wholesale_client',
-        )
-        return user
-
-
-class RegisterSalesAgentSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = User
-        fields = [
-            'username',
-            'email',
-            'phone_number',
-            'password',
-        ]
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            phone_number=validated_data['phone_number'],
-            password=validated_data['password'],
-            role='sales_agent',
-        )
-        return user
-
-
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
