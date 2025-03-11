a1bdc0042952ec1478dc677a1f429bd68d8a1be1
hypolitzeuchieu <hypolitzeuchieu@gmail.com>
2025-03-11 20:30:13 +0100

    update send and create notification

--- stockmanagement/notifications/service.py
+++ stockmanagement/notifications/service.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
 import logging
+from datetime import timedelta
 
 from asgiref.sync import async_to_sync
+from authentication.models import User
 from channels.layers import get_channel_layer
+from django.utils.timezone import now
 from notifications.models import Notification
 
 logger = logging.getLogger(__name__)
@@ -11,48 +14,67 @@ logger = logging.getLogger(__name__)
 
 class NotificationService:
     """
-    Service for sending notifications.
+    Service for sending notifications to managers only.
     """
 
     @staticmethod
-    def create_notification(user, product, notification_type, message):
+    def create_notification(product, notification_type, message):
         try:
+            time_threshold = now() - timedelta(minutes=1)
+
             existing_notification = Notification.objects.filter(
-                user=user,
                 product=product,
                 notification_type=notification_type,
-                message=message,
-            ).exists()
+                created_at__gte=time_threshold
+            ).first()
             if existing_notification:
-                logger.info(f"Notification already sent for"
-                            f" {notification_type} on {product.name}. Skipping.")
-                return None
+                logger.info(
+                    f"Notification already sent for {notification_type} "
+                    f"on {product.name}. Resending to managers.")
+                NotificationService.send_notification_to_managers(existing_notification)
+                return existing_notification
 
             notif = Notification.objects.create(
-                user=user,
                 product=product,
                 notification_type=notification_type,
                 message=message,
             )
+            notif.refresh_from_db()
 
-            channel_layer = get_channel_layer()
-            async_to_sync(channel_layer.group_send)(
-                'notifications',
-                {
-                    'type': 'send_notification',
-                    'data': {
-                        'notification_type': notif.notification_type,
-                        'message': notif.message,
-                        'product': notif.product.name,
-                        'created_at': notif.created_at.strftime('%Y-%m-%d %H:%M:%S'),
-                    },
-                },
-            )
-
+            NotificationService.send_notification_to_managers(notif)
             return notif
         except Exception as e:
             logger.error(f"Unexpected error occurred: {str(e)}")
             return {'error': f"Unexpected error occurred: {str(e)}"}
+
+    @staticmethod
+    def send_notification_to_managers(notification):
+        """
+            Sends notification only to managers via WebSockets.
+        """
+        managers = User.objects.filter(role='manager')
+        if not managers.exists():
+            logger.warning('No managers found to notify.')
+            return
+
+        print(f"Sending notification to {managers.count()} managers")
+
+        channel_layer = get_channel_layer()
+
+        for manager in managers:
+            async_to_sync(channel_layer.group_send)(
+                f'notifications_{manager.id}',
+                {
+                    'type': 'send_notification',
+                    'data': {
+                        'notification_type': notification.notification_type,
+                        'message': notification.message,
+                        'product': notification.product.name,
+                        'created_at': notification.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+                    },
+                },
+            )
+        print('Ws Notification sent to manager')
 
     @staticmethod
     def get_all_notifications():
