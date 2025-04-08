469997324dbcce3a76015d2f38708a52d80d45d2
hypolitzeuchieu <hypolitzeuchieu@gmail.com>
2025-04-08 15:20:01 +0100

    remove uniqueness constraint from number field in models

--- stockmanagement/reports/models.py
+++ stockmanagement/reports/models.py
@@ -173,7 +173,7 @@ class InvoiceArchive(models.Model):
     ]
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    number = models.PositiveIntegerField(unique=True, editable=False)
+    number = models.PositiveIntegerField()
     invoice_id = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     client_name = models.CharField(max_length=100, blank=True, null=True)
