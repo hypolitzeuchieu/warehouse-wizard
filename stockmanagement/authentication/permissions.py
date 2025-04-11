b611182fdebc4dbfebedac9d5e3d450b0c03b1bc
hypolitzeuchieu <hypolitzeuchieu@gmail.com>
2025-04-11 11:34:03 +0100

    refactor permissions to use role hierarchy for cleaner access control

--- stockmanagement/authentication/permissions.py
+++ stockmanagement/authentication/permissions.py
@@ -3,22 +3,30 @@ from __future__ import annotations
 from rest_framework.permissions import BasePermission
 
 
-class IsManagerPermission(BasePermission):
+ROLE_HIERARCHY = {
+    'manager': ['manager', 'cashier', 'storekeeper'],
+    'cashier': ['cashier'],
+    'storekeeper': ['storekeeper'],
+}
 
+
+def has_role(user, *roles):
+    if not user.is_authenticated:
+        return False
+    allowed_roles = ROLE_HIERARCHY.get(user.role, [])
+    return any(role in allowed_roles for role in roles)
+
+
+class IsManagerPermission(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'manager'
+        return has_role(request.user, 'manager')
 
 
 class IsCashier(BasePermission):
-
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'cashier'
+        return has_role(request.user, 'cashier')
 
 
 class IsStorekeeper(BasePermission):
-
     def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated
-            and request.user.role == 'storekeeper'
-        )
+        return has_role(request.user, 'storekeeper')
