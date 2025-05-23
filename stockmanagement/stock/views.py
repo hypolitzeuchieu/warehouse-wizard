ceb177048fe0094b2e4f534eae39defab50f62c1
hypolitzeuchieu <hypolitzeuchieu@gmail.com>
2025-05-23 16:02:20 +0100

    chore: increase default page size in CustomPagination to 100

--- stockmanagement/stock/views.py
+++ stockmanagement/stock/views.py
@@ -31,7 +31,7 @@ logger = logging.getLogger(__name__)
 
 class CustomPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
-    page_size = 10
+    page_size = 100
     max_page_size = 100
 
 
