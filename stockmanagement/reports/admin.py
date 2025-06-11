d59d255960fc0f44f4f078b12030ea01c2319b25
hypolitzeuchieu <hypolitzeuchieu@gmail.com>
2025-06-11 11:18:22 +0100

    feat: register invoice-related models in the admin interface

--- stockmanagement/reports/admin.py
+++ stockmanagement/reports/admin.py
@@ -1,1 +1,18 @@
 from __future__ import annotations
+
+from django.contrib import admin
+from reports.models import InventoryReport
+from reports.models import Invoice
+from reports.models import InvoiceArchive
+from reports.models import InvoiceArchiveLine
+from reports.models import InvoiceLine
+from reports.models import Report
+from reports.models import SalesReport
+
+admin.site.register(Invoice)
+admin.site.register(InvoiceLine)
+admin.site.register(Report)
+admin.site.register(InvoiceArchive)
+admin.site.register(InvoiceArchiveLine)
+admin.site.register(InventoryReport)
+admin.site.register(SalesReport)
