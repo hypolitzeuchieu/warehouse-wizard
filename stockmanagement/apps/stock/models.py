ba4a9ef8420ece7f06dc695bf5f2e7dbb5f73b2a
hypolitzeuchieu <hypolitzeuchieu@gmail.com>
2025-08-04 20:04:04 +0100

    feat: add barcode and barcode_url fields to product model

--- stockmanagement/apps/stock/models.py
+++ stockmanagement/apps/stock/models.py
@@ -50,6 +50,8 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    barcode = models.CharField(max_length=50, unique=True, null=True, blank=True)
+    barcode_url = models.URLField(max_length=255, unique=True, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     image = models.URLField(max_length=200, null=True, blank=True)
